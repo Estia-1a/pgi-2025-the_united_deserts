9b36c70c58e4d28306693f4c253b34e893635c64
MatthieuCHB <matthieu.chambon@etu.estia.fr>
2025-05-21 17:19:32 +0200

    Full report in a txt file

--- src/features.c
+++ src/features.c
@@ -1,5 +1,6 @@
 #include <estia-image.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "estia-image.h"
 #include "features.h"
@@ -15,12 +16,40 @@
 void helloWorld() {
     printf("Hello World !");
 }  
+
 void dimension(char *source_path) {
     unsigned char *data = NULL;
     int width = 0, height = 0, channels = 0;
 
-    // read_image_data va lire les données de l'image
     if (read_image_data(source_path, &data, &width, &height, &channels)) {
         printf("dimension: %d, %d\n", width, height);
     }
+}
+
+
+void stat_report(char *source_path) {
+    unsigned char *data = NULL;
+    int width = 0, height = 0, channels = 0;
+    int total_pixels = width * height;
+    int maxPix = max_pixel(data, total_pixels, channels);
+    int minPix = min_pixel(data, total_pixels, channels);
+    int maxR = max_component(data, total_pixels, channels, 0);
+    int maxG = max_component(data, total_pixels, channels, 1);
+    int maxB = max_component(data, total_pixels, channels, 2);
+    int minR = min_component(data, total_pixels, channels, 0);
+    int minG = min_component(data, total_pixels, channels, 1);
+    int minB = min_component(data, total_pixels, channels, 2);
+    FILE *f = fopen("stat_report.txt", "w");
+   
+
+    fprintf(f, "max_pixel: %d\n", maxPix);
+    fprintf(f, "min_pixel: %d\n\n", minPix);
+    fprintf(f, "max_component R: %d\n", maxR);
+    fprintf(f, "max_component G: %d\n", maxG);
+    fprintf(f, "max_component B: %d\n\n", maxB);
+    fprintf(f, "min_component R: %d\n", minR);
+    fprintf(f, "min_component G: %d\n", minG);
+    fprintf(f, "min_component B: %d\n", minB);
+    fclose(f);
+    free(data);
 }
--- src/features.h
+++ src/features.h
@@ -4,5 +4,11 @@
 
 void helloWorld();
 void dimension(char *source_path);
+void stat_report(char *source_path);
+
+int max_pixel(unsigned char *data, int total_pixels, int channels);
+int min_pixel(unsigned char *data, int total_pixels, int channels);
+int max_component(unsigned char *data, int total_pixels, int channels, int component);
+int min_component(unsigned char *data, int total_pixels, int channels, int component);
 
 #endif
